259fa9499dba55c87e7c5ef857fa6834b1438df6
BoegyNat <bboegy@gmail.com>
2022-01-11 20:16:32 +0700

    Lab15 Finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for(int i=1;i<N;i++){
+		cout << "Pass " << i << ":";
+		
+		int j= i-1;
+    	T key = d[i];
+    	while(d[j]<key && j>=0)
+    	{
+           d[j+1]=d[j];
+           j--;
+    	}
+       d[j+1]=key;
 
+		for(int j=0;j < N;j++){
+			cout << d[j] ;
+			if(j!=N-1) cout << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main(){
@@ -17,3 +34,12 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+/*int j= i-1;
+    	int key = d[i];
+    	while(d[j]<key && j>=0)
+    	{
+           d[j+1]=d[j];
+           j--;
+    	}
+       d[j+1]=key;*/
